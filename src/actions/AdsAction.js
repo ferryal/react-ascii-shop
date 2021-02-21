20ed7d1dfda7259575331da6a45c9a7240726e82
ferryal <ferryal.bahari@amartha.com>
2021-02-22 06:58:24 +0700

    add action fetch ads

---
+++ src/actions/AdsAction.js
@@ -0,0 +1,22 @@
+import { BaseUrl } from '../constants/Api';
+
+export function fetchAds() {
+  const r = Math.floor(Math.random() * 1000);
+  return async (dispatch) => {
+    fetch(`${BaseUrl}ads?r=${r}`)
+      .then((result) => {
+        if (result.status === 200) {
+          dispatch({
+            type: 'FETCH_ADS_SUCCESS',
+            payload: result.url,
+          });
+        }
+      })
+      .catch((error) => {
+        dispatch({
+          type: 'FETCH_ADS_ERROR',
+          payload: error,
+        });
+      });
+  };
+}
