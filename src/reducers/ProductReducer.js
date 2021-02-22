a95c2521bf8906734184d9ed057ee8bbd6dc18e5
ferryal <ferryal.bahari@amartha.com>
2021-02-22 07:00:43 +0700

    feat: add reducer products

---
+++ src/reducers/ProductReducer.js
@@ -0,0 +1,31 @@
+const initialState = {
+  loading: true,
+  products: [],
+  error: '',
+};
+
+export default (state = initialState, action) => {
+  const result = action.payload;
+  switch (action.type) {
+  case 'FETCH':
+    return {
+      ...state,
+      loading: true,
+    };
+  case 'FETCH_SUCCESS':
+    return {
+      ...state,
+      loading: false,
+      products: result,
+      sort: action.sort,
+    };
+  case 'FETCH_ERROR':
+    return {
+      ...state,
+      loading: false,
+      error: 'There is something wrong!',
+    };
+  default:
+    return state;
+  }
+};
